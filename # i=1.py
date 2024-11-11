34f6d0cec061ffdfb34161c801c38f5ca998fd97
Giorgi Gabekhadze <g.gabekhadze@buckswood.ge>
2024-11-11 18:06:17 +0400

    pythonex

---
+++ # i=1.py
@@ -0,0 +1,17 @@
+# i=1 
+# while i < 19:
+#     print(i, end =' ')
+#     i += 1
+
+# age = int(input("enter your age:"))
+# while 18 < age < 75:
+#     print("you can vote!")
+#     break
+
+# question = "yes"
+# while question == "yes":
+#     print("you are a good person! ")
+    
+#     question = str(input("Do you wanna hear this again? pls enter YES or NO :"))
+# if question == "no":
+#     print("THATS SAD BUT ITS k")
